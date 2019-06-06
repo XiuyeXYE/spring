4fd0d1f0f2c3e4ea178f15bfe07a9d0b266058b3
xiuye <xiuye_engineer@outlook.com>
2019-06-06 19:51:26 +0800

    new

---
+++ src/org/springframework/cglib/core/Customizer.java
@@ -0,0 +1,9 @@
+package org.springframework.cglib.core;
+
+import org.springframework.asm.Type;
+import org.springframework.cglib.core.CodeEmitter;
+import org.springframework.cglib.core.KeyFactoryCustomizer;
+
+public interface Customizer extends KeyFactoryCustomizer {
+	public void customize(CodeEmitter var1, Type var2);
+}
